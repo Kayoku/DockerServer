ec712d85f8e24f75f58097c60b232d4f26be7260
Jordan Bouchoucha <jordan.bouchoucha@protonmail.com>
2018-11-09 17:08:53 +0100

    generate.sh

--- generate.sh
+++ generate.sh
@@ -11,6 +11,16 @@ cp -R ./base/nextcloud-comp ./generate
 cp -R ./base/proxy-comp ./generate
 
 # Decrypt config and change docker-compose param
-cd ./base/private/
-./decrypt.sh
-cd -
+if [ ! -f './base/private/config.txt' ]
+then
+	cd ./base/private/
+	./decrypt.sh
+	cd -
+fi
+
+# Replace all word in ./base/private/config.txt to ./generate/nextcloud-comp/docker-compose.yml
+while read line
+do
+	IFS== read name value <<< $line
+	for i in ./generate/nextcloud-comp/docker-compose.yml; do sed -i "s/$name/$value/g" "$i"; done
+done < ./base/private/config.txt
